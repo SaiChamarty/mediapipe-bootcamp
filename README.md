537a711f5b67c5a595fd471b9bd44e4bfe38ad95
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2025-02-20 23:03:10 -0800

    Code from bootcamp and some additional README comments

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # mediapipe-bootcamp
+
+## What does it do?
+This is a program that uses a previously trained model to recognize body-pivot points, when camera is opened and a human is seen.
+
+## Important details for running
+As mediapipe is not available for the latest version of python, I used python 3.11 and run the program in a virtual environment with python 3.11:
+```
+python3.11 -m venv venv311
+```
+
+And then we can activate the virtual environment and run our code.
+```
+source ./venv311/bin/activate
+```
+## How to run the program?
+Install the dependencies:
+```
+pip install opencv-python mediapipe numpy
+```
+
+Once the dependencies are in, run the program using the following command:
+```
+python main.py
+```
--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import cv2
 import mediapipe as mp
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+cap = cv2.VideoCapture(1) # camera index number
 
-# For webcam input:
-cap = cv2.VideoCapture(2)
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened(): # while the camera is running
+        success, image = cap.read() ## these are the arguments we get from the camera
+        if not success:
+          print("Ignoring empty camera frame")
+          continue
 
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    results = pose.process(image)
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
 
-    # Draw the pose annotation on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-    cv2.imshow('MediaPipe Pose', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        results = pose.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if results.pose_landmarks:
+           mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+           h,w,z = image.shape
+
+           for idx, landmark in enumerate(results.pose_landmarks.landmark):
+              cx, cy = int(landmark.x *w) , int(landmark.y *h)
+              confidence = landmark.visibility
+
+              cv2.putText(image, f"{idx} ({confidence:.2f})", (cx, cy) , cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2, cv2.LINE_AA)
+        
+        cv2.imshow("Mediapipe pose", image)
+
+        if cv2.waitKey(5) & 0xFF == 27:
+          break
+
 cap.release()
+cv2.destroyAllWindows()
